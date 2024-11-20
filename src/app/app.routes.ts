854a35a41ff84f40ac274a229166fddd67851bbd
HungTH-Regit <hungth@regit-technology.com>
2024-11-20 17:52:23 +0700

    Create base project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'products', component: ProductComponent },
+    { path: '', component: HomeComponent },
+    { path: '**', component: HomeComponent }
+];
